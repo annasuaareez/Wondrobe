40a345fc788cda11e8a65973a2381f876e80148d
annasuaareez <a.suarez.villaescusa@gmail.com>
2024-03-26 00:52:44 +0100

    Change dark mode and light mode

--- app/src/main/java/com/example/wondrobe/Charge.kt
+++ app/src/main/java/com/example/wondrobe/Charge.kt
@@ -1,18 +1,35 @@
 package com.example.wondrobe
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.view.animation.AnimationUtils
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class Charge: AppCompatActivity() {
-    private val splashTimeOut: Long = 2000
+    private val splashTimeOut: Long = 1500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(splashTimeOut)
         installSplashScreen()
         setContentView(R.layout.activity_charge)
+
+        val buttonLogIn = findViewById<Button>(R.id.buttonLogIn)
+        val buttonSignUp = findViewById<Button>(R.id.buttonSignUp)
+
+        buttonSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }
+
+        buttonLogIn.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/wondrobe/SignUp.kt
@@ -0,0 +1,29 @@
+package com.example.wondrobe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        val isotypeImageView = findViewById<ImageView>(R.id.isotype)
+
+        // Escuchar cambios en el tema de la aplicación
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        delegate.applyDayNight()
+
+        // Cambiar la imagen de acuerdo con el tema actual
+        val currentNightMode = resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK
+        if (currentNightMode == android.content.res.Configuration.UI_MODE_NIGHT_YES) {
+            // Modo oscuro
+            isotypeImageView.setImageResource(R.drawable.isotype_white)
+        } else {
+            // Modo claro
+            isotypeImageView.setImageResource(R.drawable.isotype_black)
+        }
+    }
+}
