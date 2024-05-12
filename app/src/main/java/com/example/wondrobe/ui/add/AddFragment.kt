5ded5bc9c99029f959e059249029bf7c021e5371
annasuaareez <a.suarez.villaescusa@gmail.com>
2024-05-12 19:07:25 +0200

    Terms and Conditions updates.

--- app/src/main/java/com/example/wondrobe/ui/add/AddFragment.kt
+++ app/src/main/java/com/example/wondrobe/ui/add/AddFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.wondrobe.R
 import com.example.wondrobe.databinding.FragmentAddBinding
+import com.example.wondrobe.ui.add.post.SelectPost
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddFragment : BottomSheetDialogFragment() {
